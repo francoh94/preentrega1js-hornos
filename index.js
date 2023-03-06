87bdf7134d7bdd4976ef625a5ee1a661f548cc8e
preentrega3+Hornos <francohornos@outlook.es>
2023-03-06 14:00:18 -0300

    preentrega1js-b

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function Validacion(edad, ci, sueldo){
-    if (isNaN(edad)|| edad > 18){
+    if ((edad) && edad > 18){
         validar = true;
         console.log(edad);}
-    if (isNaN(edad)|| edad < 18){
+    if ((edad)|| edad < 18){
         validar = false;
         alert("edad no permitida");
     }
-    if (isNaN(ci) && ci > 10000000 && ci < 70000000) {
+    if ((ci) && ci > 10000000 && ci < 70000000) {
         validar = true;
         console.log(ci);}
-    if (isNaN(ci)|| ci < 10000000 || ci > 70000000) {
+    if ((ci)|| ci < 10000000 || ci > 70000000) {
         validar = false;
         alert("ci no admitida");
         
     }
-    if (isNaN(sueldo) || sueldo >= 10000 ){
+    if ((sueldo) || sueldo >= 10000 ){
         validar = true;
         alert(`${nombre} con su sueldo, tiene una garantia para un alquiler de: ${sueldo / 2}`);
     }
@@ -27,9 +27,9 @@ function Validacion(edad, ci, sueldo){
 alert("bienvenido, calculemos que alquiler le permite su sueldo.")
 let nombre = prompt("ingrese su nombre y apellido")
 console.log(nombre);
-let edad = prompt("ingrese su edad")
-let ci = prompt("ingrese su CI(sin puntos o guiones)")
-let sueldo = prompt("ingrese su sueldo liquido")
+let edad = parseInt(prompt("ingrese su edad"))
+let ci = parseInt(prompt("ingrese su CI(sin puntos o guiones)"))
+let sueldo = parseInt(prompt("ingrese su sueldo liquido"))
 Validacion(edad, ci, sueldo);
 alert("calculemos el alquiler para los siguientes 3 años")
 const alquiler = parseInt(prompt("ingresa tu alquiler"))
